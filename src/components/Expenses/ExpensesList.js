3544292df213e9793e959dbfcb42cae02883a7d0
Ryota Fujisawa <fujisawa3838@gmail.com>
2022-07-05 13:44:52 +0900

    微調整

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -8,19 +8,21 @@ const ExpensesList = (props) => {
   // JSXコンテンツを変数に格納することができます。
 
   if (props.items.length === 0) {
-    return <h2 className="expenses-list__fallback">Found no expenses.</h2>
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
 
-  return <ul className="expenses-list">
-    {props.items.map((expense) => (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        key={expense.id}
-      />
-    ))}
-  </ul>;
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          key={expense.id}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default ExpensesList;
