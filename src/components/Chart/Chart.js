64c16c08d17f9b2c0707498923cc0645659ec4de
Ryota Fujisawa <fujisawa3838@gmail.com>
2022-07-06 15:28:59 +0900

    add comment

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,6 +6,7 @@ import "./Chart.css";
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
   // このmaxメソッドには12個の引数が渡されます。この引数は、スプレッド演算子で取り出した配列から取り出した12個の値です。
+  // 例えば、[1,2,3,4]の中で最も大きい数字が取り出される
   const totalMaximum = Math.max(...dataPointValues)
 
   return (
