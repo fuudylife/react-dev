2707b8645fde114abb1fe240eaf5040245fa6b3a
Ryota Fujisawa <fujisawa3838@gmail.com>
2022-07-06 12:57:51 +0900

    add expensesChart and completed

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,17 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
+  // このmaxメソッドには12個の引数が渡されます。この引数は、スプレッド演算子で取り出した配列から取り出した12個の値です。
+  const totalMaximum = Math.max(...dataPointValues)
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -25,6 +26,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onClickPickyear={filterWithPickedyear}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
